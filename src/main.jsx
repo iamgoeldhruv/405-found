72904e2c961027712a20ad765eb8e6b9af995d1c
codebreaker0001 <yashyadav0171@gmail.com>
2023-10-28 22:22:47 +0530

    yes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Route, RouterProvider , createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
 import App from './App.jsx'
 import './index.css'
+import Trade from './components/Trade/Trade.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='/trade' element={<Trade/>} />
+     
+    </Route>
+  )
+)
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+  <RouterProvider router={router}/>
   </React.StrictMode>,
 )
